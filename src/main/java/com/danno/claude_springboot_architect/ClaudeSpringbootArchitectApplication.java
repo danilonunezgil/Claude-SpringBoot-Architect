0ef99fa19a9408accefebbc1eb3f927d18083477
danilonunezgil <danilo.gil98@gmail.com>
2026-05-03 08:22:01 -0500

    Base structure for sample

---
+++ src/main/java/com/danno/claude_springboot_architect/ClaudeSpringbootArchitectApplication.java
@@ -0,0 +1,13 @@
+package com.danno.claude_springboot_architect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeSpringbootArchitectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudeSpringbootArchitectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danno/claude_springboot_architect/ClaudeSpringbootArchitectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danno.claude_springboot_architect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudeSpringbootArchitectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
